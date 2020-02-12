dc8dd7aaa7e242b6d4419cc6bca322bfdf2a0ff1
packyzbq <packyzhao@163.com>
2020-02-12 08:47:28 +0800

    Site updated: 2020-02-12 08:47:25

---
+++ js/my.js
@@ -0,0 +1,17 @@
+/*
+type your custom JavaScript here
+在本文件中键入您的自定义JS脚本
+升级主题文件时请注意保护本文件，防止意外覆盖。
+*/
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
